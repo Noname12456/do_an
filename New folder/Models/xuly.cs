2c9916408f3d2b1e5a447c0c471d154b6997817f
KID <votuan45678@gmail.com>
2022-06-17 23:42:08 +0700

    update web

---
+++ New folder/Models/xuly.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace web.Models
+{
+    public class xuly
+    {
+        public string conf = "Data Source=KID;Initial Catalog=QL_NhaSach;User ID=sa";
+    }
+}
